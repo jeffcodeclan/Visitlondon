cc19108ac98537454e8dedc794a80a93bb576042
Jeff <jeffcodeclan@gmail.com>
2017-03-30 12:00:42 +0100

    added grid and mixins scss

--- gulpfile.js
+++ gulpfile.js
@@ -45,13 +45,9 @@ gulp.task('clean-scripts', function() {
 });
 
 gulp.task('sass', function(){
-  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
-  var sassFiles;
-
   sassFiles = gulp.src(SOURCEPATHS.sassApp)
     .pipe(autoprefixer())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    return merge(bootstrapCSS, sassFiles)
       .pipe(concat('app.css'))
       .pipe(gulp.dest(APPPATH.css));
 });
@@ -62,11 +58,6 @@ gulp.task('images', function() {
    .pipe(imagemin())
    .pipe(gulp.dest(APPPATH.img));
 })
-
-gulp.task('moveFonts', function() {
-  gulp.src('./node_modules/bootstrap/dist/fonts/*.{eot,svg,ttf,woff,woff2}')
-    .pipe(gulp.dest(APPPATH.fonts))
-});
 
 gulp.task('scripts', ['clean-scripts'], function() {
   gulp.src(SOURCEPATHS.jsSource)
@@ -86,12 +77,9 @@ gulp.task('compress', function() {
 });
 
 gulp.task('compresscss', function() {
-  var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.css');
-  var sassFiles;
   sassFiles = gulp.src(SOURCEPATHS.sassSource)
     .pipe(autoprefixer())
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    return merge(bootstrapCSS, sassFiles)
       .pipe(concat('app.css'))
       .pipe(cssmin())
       .pipe(rename({suffix: '.min'}))
@@ -130,7 +118,7 @@ gulp.task('serve', ['sass'], function() {
   })
 });
 
-gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', 'moveFonts', 'images', 'html'], function() {
+gulp.task('watch', ['serve', 'sass', 'clean-html', 'clean-scripts', 'scripts', 'images', 'html'], function() {
   gulp.watch([SOURCEPATHS.sassSource], ['sass']);
   //gulp.watch([SOURCEPATHS.htmlSource], ['copy']);
   gulp.watch([SOURCEPATHS.jsSource], ['scripts']);
